089f64ec2956bf21a1de7097c271fa3a04feb72c
Shane O'Moore <shaneiadt@gmail.com>
2022-03-06 15:21:15 +0000

    render posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link';
 import { Header } from '../components/Header'
 
 import { sanityClient, urlFor } from '../sanity';
@@ -10,7 +11,6 @@ interface Props {
 }
 
 const Home: NextPage<Props> = ({ posts }: Props) => {
-  console.log(posts);
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -28,6 +28,28 @@ const Home: NextPage<Props> = ({ posts }: Props) => {
 
         <img className="hidden md:inline-flex h-32 lg:h-full" src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="M - Logo" />
       </div>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+        {posts.map(post => (
+          <Link
+            key={post._id}
+            href={`/posts/${post.slug.current}`}
+          >
+            <div className='group cursor-pointer border rounded-lg overflow-hidden'>
+              <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()!} alt="" />
+
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>{post.description} by {post.author.name}</p>
+                </div>
+                
+                <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()} alt="" />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
@@ -40,6 +62,7 @@ export const getServerSideProps = async () => {
     title,
     description,
     mainImage,
+    slug,
     author->{
       name,
       image
