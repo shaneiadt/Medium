31ba52744a8e4c0e8b050979e7011a87a0ef7d5a
Shane O'Moore <shaneiadt@gmail.com>
2022-03-10 11:17:23 +0000

    building up post structure

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,16 +1,30 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import React from 'react'
-import { sanityClient } from '../../sanity';
+import { Header } from '../../components/Header';
+import { sanityClient, urlFor } from '../../sanity';
 import { Post } from '../../typings';
 
 interface Props {
   post: Post;
 }
 
-const Post = ({ post }: Props) => {
-  console.log(post);
+const Post = ({ post: { mainImage, title, description, author, _createdAt } }: Props) => {
   return (
-    <h1>{post.title}</h1>
+    <main>
+      <Header />
+
+      <img className='w-full h-40 object-cover' src={urlFor(mainImage).url()} alt={title} />
+
+      <article className='max-w-3xl mx-auto p-5'>
+        <h1 className='text-3xl mt-10 mb-3'>{title}</h1>
+        <h2 className='text-xl font-light text-gray-500 mb-2'>{description}</h2>
+
+        <div className='flex items-center space-x-2'>
+          <img className='h-10 w-10 rounded-full' src={urlFor(author.image).url()} alt={author.name} />
+          <p className='font-extralight text-sm'>Bloy post by <span className='text-green-600'>{author.name}</span> - Published at {new Date(_createdAt).toLocaleString()}</p>
+        </div>
+      </article>
+    </main>
   )
 }
 
